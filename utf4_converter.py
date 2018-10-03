a49dae6db9095d1a267f47a013b4f05dd94f76f5
mrahmanom <43763287+mrahmanom@users.noreply.github.com>
2018-10-03 11:37:29 -0400

    IVYDEV-1425 Updating script to take command line args

--- utf4_converter.py
+++ utf4_converter.py
@@ -1,6 +1,8 @@
 import os
 import chardet
 import glob
+import sys
+import argparse
 
 def list_files(path):
     result = []
@@ -30,8 +32,17 @@ def convert_files_to_utf8(file):
     with open(file, 'w+b') as f:
         f.write(file_data.decode('utf-16').encode('utf-8'))
 
-def __main__(**kwargs):
-    path = kwargs["path"]
+def process(path):
+    print("working on path" + path)
     files = get_files_with_utf16_encoding(path)
+    print("got list of files")
     for f in files:
         convert_files_to_utf8(f)
+    print("exiting script")
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Convert all sql scripts that are encoded in UTF-16 to UTF-8")
+    parser.add_argument('path', type=str, help="Path to parse files")
+    args = parser.parse_args()
+    process(args.path)
